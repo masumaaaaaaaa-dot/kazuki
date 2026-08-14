e96dc4b3ed554670ce2da7a23ee8079eb1565f58
masumaaaaaaaa-dot <masumaaaaaaaa@gmail.com>
2026-08-14 14:53:40 +0900

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE='nich-photo-ledger-cover-optional-v115.0.0';
-const ASSETS=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png','./icon-512-maskable.png','./photo_album_template.xlsm','./privacy.html','./screenshots/1_photos.png','./screenshots/2_board.png','./screenshots/3_output.png','./lib/jszip.min.js','./lib/html2canvas.min.js','./lib/jspdf.umd.min.js'];
+const CACHE='nich-photo-ledger-cover-optional-v116.0.0';
+const ASSETS=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png','./icon-512-maskable.png','./photo_album_template.xlsm','./privacy.html','./1_photos.png','./2_board.png','./3_output.png','./jszip.min.js','./html2canvas.min.js','./jspdf.umd.min.js'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>{

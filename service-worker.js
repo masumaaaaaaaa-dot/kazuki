819542ad287ee7c2c4da9b706738de2192c399ff
masumaaaaaaaa-dot <masumaaaaaaaa@gmail.com>
2026-08-16 15:53:50 +0900

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='nich-photo-ledger-camera-v191.0.0';
+const CACHE='nich-photo-ledger-camera-v192.0.0';
 const CORE=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png','./icon-512-maskable.png','./photo_album_template.xlsm','./privacy.html'];
 self.addEventListener('install',e=>e.waitUntil(
   caches.open(CACHE)
